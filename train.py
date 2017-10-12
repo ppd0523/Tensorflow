e496e0c72bc785e605d5cf232951d65c7994ca37
Park Donghyeon <emma0415@naver.com>
2017-10-12 18:39:12 +0900

    regresion lstm2 dnn5

--- train.py
+++ train.py
@@ -47,7 +47,7 @@ for i in range(0, len(y) - seq_length):
     dataX.append(_x)
     dataY.append(_y)
 
-trainSize = int(len(dataY) * 0.7)
+trainSize = int(len(dataY) * 0.8)
 testSize = len(dataY) - trainSize
 trainX, testX = np.array(dataX[0:trainSize]), np.array(dataX[trainSize:len(dataX)])
 trainY, testY = np.array(dataY[0:trainSize]), np.array(dataY[trainSize:len(dataX)])
@@ -56,14 +56,11 @@ X = tf.placeholder(tf.float32, shape=[None, seq_length, inputDim], name="EMGs")
 Y = tf.placeholder(tf.float32, shape=[None, 1], name="Angle")
 keep_prob = tf.placeholder(tf.float32)
 
-with tf.variable_scope('LSTM_layers'):
-
+with tf.variable_scope('LSTM_layer1'):
     cell = tf.nn.rnn_cell.LSTMCell(num_units=inputDim, state_is_tuple=True, activation=tf.tanh)
     cells = tf.nn.rnn_cell.MultiRNNCell([cell] * RNNLayers, state_is_tuple=True)
     outputs, _states = tf.nn.dynamic_rnn(cells, X, dtype=tf.float32)
     X_FC = tf.contrib.layers.fully_connected(outputs[:,-1], hiddenDim1, activation_fn=None)
-
-    tf.summary.histogram("X_FC", X_FC)
 
 with tf.variable_scope('Fully_connected_layer1'):
     W1_FC = tf.get_variable("W1_FC", [hiddenDim1, hiddenDim2], tf.float32, initializer=xavier_init(hiddenDim1, hiddenDim2))
@@ -107,8 +104,8 @@ with tf.variable_scope('Fully_connected_layer4'):
     H4_FC = tf.nn.relu(tf.matmul(H3_FC, W4_FC) + B4_FC, name="H4_FC")
     H4_FC = tf.nn.dropout(H4_FC, keep_prob=keep_prob)
 
-    tf.summary.histogram("W4_FC", W2_FC)
-    tf.summary.histogram("B4_FC", B2_FC)
+    tf.summary.histogram("W4_FC", W4_FC)
+    tf.summary.histogram("B4_FC", B4_FC)
     tf.summary.histogram("H4_FC", H4_FC)
 
 with tf.variable_scope('Fully_connected_layer5'):
@@ -120,6 +117,13 @@ with tf.variable_scope('Fully_connected_layer5'):
 
     tf.summary.histogram("W5_FC", W5_FC)
     tf.summary.histogram("B5_FC", B5_FC)
+    tf.summary.histogram("hypothesis", Y_pred)
+
+loss = tf.reduce_sum(tf.square(Y_pred - Y))
+optimizer = tf.train.AdamOptimizer(learningRate).minimize(loss)
+
+last_epoch = tf.Variable(0, name="last_epoch")
+
     tf.summary.histogram("hypothesis", Y_pred)
 
 loss = tf.reduce_sum(tf.square(Y_pred - Y))
@@ -181,7 +185,7 @@ with tf.Session() as sess:
         print("Epoch= {:>5}, loss= {:>12}, RMSE: {}".format(epoch+1, avg_cost, rmse_val))
 
     # Test step
-    test_predict = sess.run(Y_pred, feed_dict={X: testX})
+    test_predict = sess.run(Y_pred, feed_dict={X: testX, keep_prob:1})
     rmse_val = sess.run(rmse, feed_dict={targets: testY, predictions: test_predict})
     print("RMSE: {}".format(rmse_val))
 
