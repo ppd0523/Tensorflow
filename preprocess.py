a9ea1814413e3094f05720a5ae31d7cd6e685470
Park Donghyeon <emma0415@naver.com>
2017-10-18 12:35:45 +0900

    LSTM-2, DNN-5

--- preprocess.py
+++ preprocess.py
@@ -1,31 +1,31 @@
 import numpy as np
 import signal_function as sf
 
-filename = "1n_angle_zeroADC"
+filename = "20171018-10min3"
 prefix = ""
-suffix = "_rms"
+suffix = "_MovSD"
 
 before = "./emg/" + filename + ".txt"
 after = "./emg/" + prefix + filename + suffix + ".txt"
 WINDOW_SIZE = 10
 
-raw = np.loadtxt(before, delimiter=" ")
+raw = np.loadtxt(before, delimiter=",")
 
-d0 = sf.MovRMS(WINDOW_SIZE)
-d1 = sf.MovRMS(WINDOW_SIZE)
-d2 = sf.MovRMS(WINDOW_SIZE)
-d3 = sf.MovRMS(WINDOW_SIZE)
+d0 = sf.MovSD(WINDOW_SIZE)
+d1 = sf.MovSD(WINDOW_SIZE)
+d2 = sf.MovSD(WINDOW_SIZE)
+d3 = sf.MovSD(WINDOW_SIZE)
 
 data = []
 
 for row in raw:
-    d0.update(row[0])
-    d1.update(row[1])
-    d2.update(row[2])
-    d3.update(row[3])
-    #angle = round(row[4] / 11.3778, 0)
-    data.append( [d0.rms, d1.rms, d2.rms, d3.rms, row[4]] )
+    d0.update((row[0]-2048))
+    d1.update((row[1]-2048))
+    d2.update((row[2]-2048))
+    d3.update((row[3]-2048))
+    angle = round(row[4] / 11.3778, 0)
+    data.append( [d0.sd, d1.sd, d2.sd, d3.sd, angle] )
 
     # data.append( [row[0]-2048, row[1]-2048, row[2]-2048, row[3]-2048, row[4] ] )
 
-np.savetxt(after, data, '%d')
+np.savetxt(after, data, '%f')
--- train.py
+++ train.py
@@ -18,29 +18,29 @@ if "DISPLAY" not in os.environ:
 
 import matplotlib.pyplot as plt
 
-training_epochs = 1000
-learningRate = 0.01
-batchSize = 50
+training_epochs = 23
+learningRate = 0.005
+batchSize = 1000
 
 inputDim = 4
-RNNLayers = 2
+RNNLayers = 1
 seq_length = 10
 hiddenDim1 = 50
-hiddenDim2 = 100
-hiddenDim3 = 100
-# hiddenDim4 = 100
-# hiddenDim5 = 50
+hiddenDim2 = 200
+hiddenDim3 = 500
+hiddenDim4 = 300
+hiddenDim5 = 200
 num_classes = 130
 
-CHECK_POINT_DIR = TB_SUMMARY_DIR = "./log/L2-10seq-50-100-100-130"
+CHECK_POINT_DIR = TB_SUMMARY_DIR = "./log/L1_10-50-200-500-300-200-130"
 
-xy = np.loadtxt('./emg/1n_angle_zeroADC_sd.txt', delimiter=' ')
+xy = np.loadtxt('./emg/20171018-MovSD-12.txt', delimiter=' ')
 
-# x = xy[50:, :-1]
-# y = xy[50:, [-1]]
+x = xy[50:, :-1]
+y = xy[50:, [-1]]
 
-x = xy[:, :-1]
-y = xy[:, [-1]]
+# x = xy[:, :-1]
+# y = xy[:, [-1]]
 
 dataX = []
 dataY = []
@@ -50,7 +50,7 @@ for i in range(0, len(y) - seq_length):
     dataX.append(_x)
     dataY.append(_y)
 
-trainSize = int(len(dataY) * 0.7)
+trainSize = int(len(dataY) * 0.99)
 testSize = len(dataY) - trainSize
 trainX, testX = np.array(dataX[0:trainSize]), np.array(dataX[trainSize:len(dataX)])
 trainY, testY = np.array(dataY[0:trainSize]), np.array(dataY[trainSize:len(dataY)])
@@ -87,57 +87,61 @@ with tf.variable_scope('Fully_connected_layer2'):
                             initializer=xavier_init(1, hiddenDim3))
     H2_FC = tf.nn.relu(tf.matmul(H1_FC, W2_FC) + B2_FC, name="H1_FC")
     H2_FC = tf.nn.dropout(H2_FC, keep_prob=keep_prob)
+    # Y_pred = tf.matmul(H1_FC, W2_FC) + B2_FC
 
     tf.summary.histogram("W2_FC", W2_FC)
     tf.summary.histogram("B2_FC", B2_FC)
     tf.summary.histogram("H2_FC", H2_FC)
+    # tf.summary.histogram("hypothis", Y_pred)
 
 with tf.variable_scope('Fully_connected_layer3'):
-    W3_FC = tf.get_variable("W3_FC", [hiddenDim3, num_classes], tf.float32,
-                            initializer=xavier_init(hiddenDim3, num_classes))
-    B3_FC = tf.get_variable("B3_FC", [num_classes], tf.float32,
-                            initializer=xavier_init(1, num_classes))
-    # H3_FC = tf.nn.relu(tf.matmul(H2_FC, W3_FC) + B3_FC, name="H3_FC")
-    # H3_FC = tf.nn.dropout(H3_FC, keep_prob=keep_prob)
-    Y_pred = tf.matmul(H2_FC, W3_FC) + B3_FC
+    W3_FC = tf.get_variable("W3_FC", [hiddenDim3, hiddenDim4], tf.float32,
+                            initializer=xavier_init(hiddenDim3, hiddenDim4))
+    B3_FC = tf.get_variable("B3_FC", [hiddenDim4], tf.float32,
+                            initializer=xavier_init(1, hiddenDim4))
+    H3_FC = tf.nn.relu(tf.matmul(H2_FC, W3_FC) + B3_FC, name="H3_FC")
+    H3_FC = tf.nn.dropout(H3_FC, keep_prob=keep_prob)
+    # Y_pred = tf.matmul(H2_FC, W3_FC) + B3_FC
 
 
     tf.summary.histogram("W3_FC", W3_FC)
     tf.summary.histogram("B3_FC", B3_FC)
-    tf.summary.histogram("H3_FC", Y_pred)
+    tf.summary.histogram("H3_FC", H3_FC)
 
-# with tf.variable_scope('Fully_connected_layer4'):
-#     W4_FC = tf.get_variable("W4_FC", [hiddenDim4, hiddenDim5], tf.float32,
-#                             initializer=xavier_init(hiddenDim4, hiddenDim5))
-#     B4_FC = tf.get_variable("B4_FC", [hiddenDim5], tf.float32,
-#                             initializer=xavier_init(hiddenDim5, hiddenDim5))
-#     H4_FC = tf.nn.relu(tf.matmul(H3_FC, W4_FC) + B4_FC, name="H4_FC")
-#     H4_FC = tf.nn.dropout(H4_FC, keep_prob=keep_prob)
-#
-#     tf.summary.histogram("W4_FC", W2_FC)
-#     tf.summary.histogram("B4_FC", B2_FC)
-#     tf.summary.histogram("H4_FC", H4_FC)
-#
-# with tf.variable_scope('Fully_connected_layer5'):
-#     W5_FC = tf.get_variable("W5_FC", [hiddenDim5, outputDim], tf.float32,
-#                             initializer=xavier_init(hiddenDim5, outputDim))
-#     B5_FC = tf.get_variable("B5_FC", [outputDim], tf.float32,
-#                             initializer=xavier_init(outputDim, outputDim))
-#     Y_pred = tf.matmul(H4_FC, W5_FC)+ B5_FC
-#
-#     tf.summary.histogram("W5_FC", W5_FC)
-#     tf.summary.histogram("B5_FC", B5_FC)
-#     tf.summary.histogram("hypothesis", Y_pred)
+with tf.variable_scope('Fully_connected_layer4'):
+    W4_FC = tf.get_variable("W4_FC", [hiddenDim4, hiddenDim5], tf.float32,
+                            initializer=xavier_init(hiddenDim4, hiddenDim5))
+    B4_FC = tf.get_variable("B4_FC", [hiddenDim5], tf.float32,
+                            initializer=xavier_init(num_classes, hiddenDim5))
+    H4_FC = tf.nn.relu(tf.matmul(H3_FC, W4_FC) + B4_FC, name="H4_FC")
+    H4_FC = tf.nn.dropout(H4_FC, keep_prob=keep_prob)
+    # Y_pred = tf.matmul(H3_FC, W4_FC) + B4_FC
+
+    tf.summary.histogram("W4_FC", W4_FC)
+    tf.summary.histogram("B4_FC", B4_FC)
+    tf.summary.histogram("H4_FC", H4_FC)
+
+with tf.variable_scope('Fully_connected_layer5'):
+    W5_FC = tf.get_variable("W5_FC", [hiddenDim5, num_classes], tf.float32,
+                            initializer=xavier_init(hiddenDim5, num_classes))
+    B5_FC = tf.get_variable("B5_FC", [num_classes], tf.float32,
+                            initializer=xavier_init(num_classes, num_classes))
+    Y_pred = tf.matmul(H4_FC, W5_FC)+ B5_FC
+
+    tf.summary.histogram("W5_FC", W5_FC)
+    tf.summary.histogram("B5_FC", B5_FC)
+    tf.summary.histogram("hypothesis", Y_pred)
 
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Y_pred, labels=Y_one_hot))
 correct_prediction = tf.equal(tf.argmax(Y_pred, 1), tf.argmax(Y_one_hot, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
 optimizer = tf.train.AdamOptimizer(learningRate).minimize(cost)
 
-last_epoch = tf.Variable(0, name="last_epoch")
+rmse2 = tf.square(tf.argmax(Y_pred, 1) - tf.argmax(Y_one_hot, 1))
+rmse = tf.sqrt(tf.reduce_mean( tf.cast(rmse2, tf.float32)))
 
+last_epoch = tf.Variable(0, name="last_epoch")
 tf.summary.scalar("loss", cost)
 
 with tf.Session() as sess:
@@ -165,41 +169,33 @@ with tf.Session() as sess:
     total_batch = math.ceil(trainSize/batchSize)
 
     for epoch in range(start_from, training_epochs):
-
+        batch_cost = 0
         # Training step
         for i in range(total_batch):
             batchX, batchY = trainX[i*batchSize:(i+1)*batchSize], trainY[i*batchSize:(i+1)*batchSize]
-
             feed_dict = {X: batchX, Y: batchY, keep_prob:0.7}
             s, _ = sess.run([summary,optimizer], feed_dict=feed_dict)
             writer.add_summary(s, global_step=global_step)
             global_step += 1
 
-        loss, acc = sess.run([cost, accuracy], feed_dict={X: testX, Y: testY, keep_prob:0.7})
-        print("loss:{:>10}, acc:{:.3%}".format(loss, acc))
+            batch_cost += sess.run(cost, feed_dict=feed_dict) / batchSize
+
+        test_rmse, acc = sess.run([rmse, accuracy], feed_dict={X: testX, Y: testY, keep_prob:1})
+
+        print("epoch:{:5>}, loss:{:>10}, acc:{}, test_rmse:{}".format(epoch, batch_cost, acc, test_rmse))
 
         sess.run(last_epoch.assign(epoch+1))
         if not os.path.exists(CHECK_POINT_DIR):
             os.makdirs(CHECK_POINT_DIR)
         saver.save(sess, CHECK_POINT_DIR + "/model", global_step=epoch+1)
 
-        pred = sess.run([cost, accuracy], feed_dict={X: testX, Y: testY, keep_prob:1.0})
-        for p, y in zip(pred, testY.flatten()):
-            print("[{}] Prediction: {:>5} True Y: {:>3}".format(p == int(y), p, int(y)))
-
-    correct_prediction = tf.equal(tf.argmax(Y_pred, 1), tf.argmax(Y_one_hot, 1))
-
-    # Test step
-    predction, real = sess.run([tf.argmax(Y_pred,1), tf.argmax(Y_one_hot,1)], feed_dict={X: testX, Y:testY, keep_prob:1})
-    # test_predict = sess.run(Y_pred, feed_dict={X: testX})
-    # rmse_val = sess.run(rmse, feed_dict={targets: testY, predictions: test_predict})
-    # print("RMSE: {}".format(rmse_val))
-
     # Plot predictions
-    # plt.plot(testY, 'r')
-    # plt.plot(test_predict, 'g')
-    plt.plot(predction)
-    plt.plot(real)
+    y = tf.argmax(Y_one_hot, 1)
+    y_ = tf.argmax(Y_pred, 1)
+
+    yp, y_p = sess.run([y, y_], feed_dict={X: testX, Y: testY, keep_prob: 1})
+    plt.plot(yp, 'g')
+    plt.plot(y_p, 'r')
     plt.xlabel("Time Period")
     plt.ylabel("Elbow Angle")
     plt.show()
